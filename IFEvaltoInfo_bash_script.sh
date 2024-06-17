66e3a39e1e69ef23aecdbf4edad2e6d9fdfd8cdb
Baidyanath Kundu <kundubaidya99@gmail.com>
2024-06-17 16:45:05 +0200

    Refactor IFEvaltoInfo

---
+++ IFEvaltoInfo_bash_script.sh
@@ -0,0 +1,1 @@
+python InfoBench/evaluation.py --api_key "ollama" --model "llama3" --input datasets/IfevalToInfo/response/IFEvaltoInfo/llama3/output.jsonl --output_dir datasets/IfevalToInfo/response/IFEvaltoInfo/llama3 --temperature 0

1f4414328c9ae21df6892352fac81f72fbc35347
Baidyanath Kundu <kundubaidya99@gmail.com>
2024-06-18 01:14:39 +0200

    Add more examples in test_chat.py

--- test_chat.py
+++ test_chat.py
@@ -1,3 +1,15 @@
 from utils.chat import chat
+import json
 
-print(chat("llama3", message="Do you know about ChatGPT?"))
+with open("config.json", 'r') as file:
+    data = json.load(file)
+    for model in data["models"]:
+        response = chat(model, message="Do you know about ChatGPT?")              # Single message API
+        print(f"Model: {model}\tType: Single Message\nResponse:\n{response}")
+
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Do you know about ChatGPT?"},
+        ]
+        response = chat(model, messages=messages)                                 # Chat format API
+        print(f"Model: {model}\tType: Chat format\nResponse:\n{response}")

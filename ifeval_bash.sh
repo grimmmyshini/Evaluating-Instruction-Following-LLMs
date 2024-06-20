dac592ee2eafd137a6d0eeeaf7f81dd8f61f534e
Piyushi Goyal <piyushigoyal@Piyushis-MacBook-Pro.local>
2024-06-20 21:13:07 +0530

    mathwell and mmlu ifeval

---
+++ ifeval_bash.sh
@@ -0,0 +1,4 @@
+python3 -m instruction_following_eval.evaluation_main \
+  --input_data=datasets/MMLU_Ifeval/mmlu_ifval.jsonl \
+  --input_response_data=datasets/MMLU_Ifeval/response/mmlu_ifval/mistral/output.jsonl \
+  --output_dir=datasets/MMLU_Ifeval/response/mmlu_ifval/mistral

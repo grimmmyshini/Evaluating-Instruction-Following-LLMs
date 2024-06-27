9611bb055a4434a4d7cbc269471af48ed9d68b03
Baidyanath Kundu <kundubaidya99@gmail.com>
2024-06-27 23:24:26 +0200

    Add run support in partwise and reordering

--- ifeval_partwise_gen_response.py
+++ ifeval_partwise_gen_response.py
@@ -14,7 +14,7 @@ def read_single_line(file):
     return None
 
 # Function to process data one line at a time and create a new JSONL file
-def process_data(input_file_path, output_file_path, model, aid=False):
+def process_data(input_file_path, output_file_path, model, mode="all"):
     # Ensure the output file exists and create an empty set for processed prompts
     if os.path.exists(output_file_path):
         with open(output_file_path, 'r') as file:
@@ -41,15 +41,24 @@ def process_data(input_file_path, output_file_path, model, aid=False):
             messages = []
             response = None
             response_list = []
-            for i, instr in enumerate(item['prompt_list']):
-                if aid and i > 1:
-                    content = instr.strip() + "\nFollow all pervious requests as well."
-                else:
-                    content = instr.strip()
-                messages.append({"role": "user", "content": content})
+
+            if mode == "aided":
+                aid = True
+            
+            if mode == "all":
+                messages = [{"role": "user", "content": prompt}]
                 response = chat(model, messages=messages)
                 response_list.append(response)
-                messages.append({"role": "assistant", "content": response})
+            else:
+                for i, instr in enumerate(item['prompt_list']):
+                    if aid and i > 1:
+                        content = instr.strip() + "\nFollow all pervious requests as well."
+                    else:
+                        content = instr.strip()
+                    messages.append({"role": "user", "content": content})
+                    response = chat(model, messages=messages)
+                    response_list.append(response)
+                    messages.append({"role": "assistant", "content": response})
 
             new_entry = {
                 "prompt": prompt,
@@ -62,7 +71,7 @@ def process_data(input_file_path, output_file_path, model, aid=False):
 
             
 
-def generate_response(input_file_path, output_path, aid=False):
+def generate_response(input_file_path, output_path, run=None, mode="all"):
     with open("config.json", 'r') as file:
         data = json.load(file)
 
@@ -72,21 +81,29 @@ def generate_response(input_file_path, output_path, aid=False):
             print(f"Running model {model}")
             output_dir_path = output_path / model
             output_dir_path.mkdir(exist_ok=True)
-            output_file_path = output_dir_path / "output.jsonl"
-            process_data(input_file_path, output_file_path, model, aid)
+            if run is None:
+                output_file_path = output_dir_path / "output.jsonl"
+            else:
+                run_path = output_dir_path / f"run{run}"
+                run_path.mkdir(exist_ok=True)
+                output_file_path = run_path / "output.jsonl"
+            process_data(input_file_path, output_file_path, model, mode=mode)
 
 # input_path = Path('datasets/MMLU_Ifeval_Complex')
 # output_path = Path('datasets/MMLU_Ifeval_Complex/response')
 
 input_dirs = ['datasets/ReorderingAnalysis_Ifeval']
 
-for input_dir in input_dirs:
-    print(f"Generating responses for jsonl files in {input_dir} directory")
-    input_path = Path(input_dir)
-    response_path = input_path / "response"
-    response_path.mkdir(exist_ok=True)
-    for input_file_path in input_path.glob("*.jsonl"):
-        output_path = response_path / (input_file_path.stem + "_partwise")
-        generate_response(input_file_path, output_path, aid=False)
-        output_path = response_path / (input_file_path.stem + "_partwise_aided")
-        generate_response(input_file_path, output_path, aid=True)
+for run in range(1, 6):
+    for input_dir in input_dirs:
+        print(f"Generating responses for jsonl files in {input_dir} directory")
+        input_path = Path(input_dir)
+        response_path = input_path / "response"
+        response_path.mkdir(exist_ok=True)
+        for input_file_path in input_path.glob("*.jsonl"):
+            output_path = response_path / input_file_path.stem
+            generate_response(input_file_path, output_path, run=run)
+            output_path = response_path / (input_file_path.stem + "_partwise")
+            generate_response(input_file_path, output_path, run=run, mode="step")
+            output_path = response_path / (input_file_path.stem + "_partwise_aided")
+            generate_response(input_file_path, output_path, run=run, mode="aided")
